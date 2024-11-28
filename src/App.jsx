10310c5b85d795f444629ff9fb0d2885e13d50ed
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-11-28 14:43:08 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
 
 function App() {
+  const [data,setData]  = useState([]);
+  const [page,setPage] = useState(1)
 
+  useEffect(() => {
+      fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}`)
+      .then(response => response.json())
+      .then(newData => setData((prevData) => [...prevData,...newData]))
+      .catch(error => console.error(error)) 
+  },[page])
+  
   return (
    <>
-
+      {
+        data.map(d => (
+           <>
+            <p>{d.body}</p>
+            <hr />
+          </>
+        ))
+      }
    </>
   )
 }
